3c19e4eac414bdba9ccb627c68139ef20c0c9ceb
joensumin <soomis7@naver.com>
2022-02-16 21:02:29 +0900

    API Key 발급 및 세팅하기

---
+++ Stock/Repository/StockRepository.swift
@@ -0,0 +1,12 @@
+//
+//  StockRepository.swift
+//  Stock
+//
+//  Created by Terry on 2022/02/16.
+//
+
+import Foundation
+
+protocol StockRepository {
+    var apiKey: String { get set }
+}
---
+++ Stock/Repository/StockRepositoryImpl.swift
@@ -0,0 +1,14 @@
+//
+//  StockRepositoryImpl.swift
+//  Stock
+//
+//  Created by Terry on 2022/02/16.
+//
+
+import Foundation
+
+class StockRepositoryImpl: StockRepository {
+    var apiKey: String = "78NIHQJ92C353EDQ"
+    
+    
+}
